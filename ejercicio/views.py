ad804de87567a428d74a1f01ac77a8a5d61a4868
Cesar <20233tn076@utez.edu.mx>
2026-03-10 15:15:04 -0600

    feat

--- ejercicio/views.py
+++ ejercicio/views.py
@@ -1,15 +1,46 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from .forms import UniversidadForm
 
-def registro(request):
 
-    if request.method == "POST":
+def formulario_view(request):
+
+    if request.method == 'POST':
+
         form = UniversidadForm(request.POST)
+
         if form.is_valid():
-            form.save()
-            return redirect("exito")
+            print("FORMULARIO VÁLIDO")
+
+            # Guardar en la base de datos
+            universidad = form.save()
+
+            # Mostrar datos guardados en consola
+            for campo, valor in form.cleaned_data.items():
+                print(f"{campo}: {valor}")
+
+            return render(
+                request,
+                'ejercicio/formulario.html',
+                {
+                    'form': UniversidadForm(),  # limpiar formulario
+                    'success': True,
+                    'usuario': universidad
+                }
+            )
+
+        else:
+            print("FORMULARIO INVÁLIDO")
+            print(form.errors)
 
     else:
         form = UniversidadForm()
 
-    return render(request, "registro.html", {"form": form})
+    return render(
+        request,
+        'ejercicio/formulario.html',
+        {'form': form}
+    )
+
+
+def scroll_view(request):
+    return render(request, 'ejercicio/scroll.html')
